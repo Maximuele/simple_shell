92cb4613fb13e29a2552ae12840bcd2948d62541
David Ooko <davidomondiooko03@gmail.com>
2024-02-19 08:02:37 -0800

    the infinite loop

---
+++ loop.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * loop - the infinite loop
+ * @program: pointer to the program name
+ *
+ * Return: void
+ *
+ */
+void loop(char *programe)
+{
+	while(1)
+	{
+		our_prompt();
+	}
+}
