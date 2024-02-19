336109b79d30214bcb7e05856e5f46d494c9c6fd
David Ooko <davidomondiooko03@gmail.com>
2024-02-19 08:09:43 -0800

    added main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - entry point to the program
+ *
+ * @argc: argument counter
+ * @argv: argument pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int interactive;
+
+	char *program;
+
+	if (argc > 0)
+	{
+		programe = argv[0];
+	}
+	else
+	{
+		program = "hsh";
+	}
+
+	return (0);
+}
